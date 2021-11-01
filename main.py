2876a06dda3be2870323706981b8f779c37d8f29
Nogstrol <spy-ru@mail.ru>
2021-11-01 11:26:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+# Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+
+# See PyCharm help at
